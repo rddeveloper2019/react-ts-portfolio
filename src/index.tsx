e84765778b2368a2a78b5260d1f4813ca5806417
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2025-07-14 21:40:53 +0300

    add esbuild form

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,52 @@
 import ReactDOM from "react-dom/client";
+import { useEffect, useRef, useState } from "react";
+import * as esbuild from "esbuild-wasm";
 
 const el = document.getElementById("root");
 
 const root = ReactDOM.createRoot(el!);
 
 const App = () => {
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+  const serviceRef = useRef<esbuild.Service | null>(null);
+
+  const initService = async () => {
+    const service = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+
+    serviceRef.current = service;
+  };
+
+  useEffect(() => {
+    initService();
+  }, []);
+
+  const onCLick = async () => {
+    if (!serviceRef.current) {
+      return;
+    }
+
+    const output = await serviceRef.current.transform(input, {
+      loader: "jsx",
+      target: "es2015",
+    });
+
+    setCode(output.code);
+  };
+
   return (
     <div>
-      <h1>Hi there!</h1>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onCLick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
     </div>
   );
 };
